84d93fa54f51e082f7cd0b4ede1c874dca32a991
Ethan <425734130@qq.com>
2017-12-31 19:34:45 +0800

    search页面组件初步完成

--- src/pages/category/category.js
+++ src/pages/category/category.js
@@ -52,7 +52,14 @@ new Vue({
   },
   filters:{
     number(price){
-      return price+'.00'
+      let priceStr = '' + price
+      if(priceStr.indexOf('.')>-1){
+        let arr = priceStr.split('.')
+        return arr[0] + '.' + (arr[1]+ '0').substr(0,2)
+      }
+      else {
+        return priceStr + '.00'
+      }
     }
   }
 
--- src/pages/search/search.js
+++ src/pages/search/search.js
@@ -1,1 +1,29 @@
 import './search.css'
+import 'css/common.css'
+
+import Vue from 'vue'
+import axios from 'axios'
+import url from 'js/api.js'
+import qs from 'qs'
+
+let {keyword,id} = qs.parse(location.search.substr(1))
+
+new Vue({
+  el: '.container',
+  data(){
+    return {
+      searchList: null,
+      keyword
+    }
+  },
+  created(){
+    this.getSearchList()
+  },
+  methods:{
+    getSearchList(){
+      axios.post(url.searchList,{keyword,id}).then(res => {
+        this.searchList = res.data.lists
+      })
+    }
+  }
+})
