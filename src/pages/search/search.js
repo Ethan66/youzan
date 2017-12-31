87e1c8f7c2dc0ea1f587c1b4706268463bb1d020
Ethan <425734130@qq.com>
2017-12-31 19:57:49 +0800

    search页面也用mixin.js处理价格问题

--- src/pages/search/search.js
+++ src/pages/search/search.js
@@ -6,6 +6,8 @@ import axios from 'axios'
 import url from 'js/api.js'
 import qs from 'qs'
 
+import mixin from 'js/mixin.js'
+
 let {keyword,id} = qs.parse(location.search.substr(1))
 
 new Vue({
@@ -25,5 +27,6 @@ new Vue({
         this.searchList = res.data.lists
       })
     }
-  }
+  },
+  mixins: [mixin]
 })
