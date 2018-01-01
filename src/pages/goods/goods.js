509b725f004bce338491753ce4df73d21eeba6f1
Ethan <425734130@qq.com>
2018-01-01 14:38:02 +0800

    商品详情页初始化

---
+++ src/pages/goods/goods.js
@@ -0,0 +1,18 @@
+
+import './goods.css'
+import './goods_base.css'
+import './goods_common.css'
+import './goods_mars.css'
+import './goods_sku.css'
+import './goods_theme.css'
+import 'css/common.css'
+
+import Vue from 'vue'
+import axios from 'axios'
+import mixin from 'js/mixin'
+import qs from 'qs'
+
+new Vue({
+  el: '#app',
+  name: '',
+})
